4f93aaed836b9610aedd14da18105bc1433236a3
Marceau Azzopardi <m.azzopardi@autobiz.com>
2021-12-17 16:03:31 +0100

    add private sell link utm tracking

--- src/config/autocasion.ts
+++ src/config/autocasion.ts
@@ -7,7 +7,7 @@ const config: TClientConfig = {
     identifier: 'autocasion',
     name: 'Autocasion',
     privateSellLink:
-        'https://www.autocasion.com/vender-coche/coche-de-segunda-mano?make=[make]&year=[year]&month=[month]',
+        'https://www.autocasion.com/vender-coche/coche-de-segunda-mano?make=[make]&year=[year]&month=[month]&utm_source=autobiz&utm_medium=iframe&utm_campaign=tasacion',
     displayRegistrationOption: false,
     registrationRegex: '^[0-9]{4}[a-zA-Z]{3}$|^[a-zA-Z]{1,2}[0-9]{4}-?[a-zA-Z]{2}$',
     zipCodeRegex: '^[0-9]{5}$|^(AD)[0-9]{3}$',
--- src/config/autoscout24.ts
+++ src/config/autoscout24.ts
@@ -7,7 +7,7 @@ const config: TClientConfig = {
     identifier: 'autoscout24',
     name: 'AutoScout24',
     privateSellLink:
-        'https://www.autoscout24.es/vender-coche/?make=[make]&year=[year]&month=[month]',
+        'https://www.autoscout24.es/vender-coche/?make=[make]&year=[year]&month=[month]utm_source=autobiz&utm_medium=iframe&utm_campaign=tasacion',
     displayRegistrationOption: false,
     registrationRegex: '^[0-9]{4}[a-zA-Z]{3}$|^[a-zA-Z]{1,2}[0-9]{4}-?[a-zA-Z]{2}$',
     zipCodeRegex: '^[0-9]{5}$|^(AD)[0-9]{3}$',
